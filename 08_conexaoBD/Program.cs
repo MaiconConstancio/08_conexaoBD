e5a7b41423041fab6bde7720e069e9837dbdeb88
maicon.sconstancio <maicon.sconstancio@CAR0588319W10-1>
2023-08-16 17:28:57 -0300

    Adicionar arquivos de projeto.

---
+++ 08_conexaoBD/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _08_conexaoBD;
+
+//ConexaoSimples conexaoSimples = new ConexaoSimples();
+
+ConexaoFlexivel conexaoFlexivel = new ConexaoFlexivel();
+conexaoFlexivel.executaQuery("SELECT * FROM tarefas");
+conexaoFlexivel.executaQuery("SELECT * FROM tarefas WHERE id = 4;");
+conexaoFlexivel.executaQuery("SELECT * FROM tarefas WHERE descricao LIKE '%tud%';");
+conexaoFlexivel.executaQuery("SELECT * FROM tarefas WHERE id;");
+
+Console.ReadKey();
---
+++ 08_conexaoBD/conexaoFlexivel.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_conexaoBD
+{
+    internal class ConexaoFlexivel
+    {
+        string host = "localhost";
+        string banco = "08_lista_tarefas";
+        string usuario = "root";
+        string senha = "senac";
+
+        MySqlConnection conexao;
+
+        public ConexaoFlexivel()
+        {
+            string dadosConexao = $"Server={host}; Database={banco}; Uid = {usuario}; Pwd={senha};";
+            conexao = new MySqlConnection(dadosConexao);
+            
+            
+        }
+        public void executaQuery( string query)
+        {
+            conexao.Open();
+
+            MySqlCommand comando = new MySqlCommand(query, conexao);
+            MySqlDataReader dados = comando.ExecuteReader();
+
+            Console.WriteLine("---------------");
+            if (dados.HasRows == false)
+            {
+                Console.WriteLine("Nenhum resultado encontrado :z ");
+            }
+
+            while (dados.Read() == true)
+            {
+                Console.WriteLine($"Descrição da tarefa {dados[0]} - {dados["descricao"]}");
+            }
+
+
+            conexao.Close();
+        }
+    
+    
+    }
+}
